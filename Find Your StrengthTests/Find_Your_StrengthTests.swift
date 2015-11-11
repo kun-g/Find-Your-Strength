0e22aa1cf3c06cb36b4c25fedc7be2a0f850fbf0
Ken <nukoug@gmail.com>
2015-11-11 10:49:45 +0800

    Initial Commit

---
+++ Find Your StrengthTests/Find_Your_StrengthTests.swift
@@ -0,0 +1,36 @@
+//
+//  Find_Your_StrengthTests.swift
+//  Find Your StrengthTests
+//
+//  Created by 郭坤 on 15/11/11.
+//  Copyright © 2015年 Lambda Theory. All rights reserved.
+//
+
+import XCTest
+@testable import Find_Your_Strength
+
+class Find_Your_StrengthTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
